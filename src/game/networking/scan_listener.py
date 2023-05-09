4f62b25dc4982bc643669d6a378371f0dfd0ea14
Yuesubi <mr.lukse@gmail.com>
2023-05-09 11:40:13 +0200

    Ajout de commentaires pour le ScanListener

--- src/game/networking/scan_listener.py
+++ src/game/networking/scan_listener.py
@@ -6,12 +6,18 @@ from .network import SELF_IP
 
 
 class ScanListener:
+    """
+    Classe pour répondre au clients qui essayent de se connecter en scannant le
+    réseau.
+    """
 
     def __init__(self) -> None:
+        """Constructeur."""
         self._should_stop: bool = False
         self._threads: set[threading.Thread] = set()
 
     def start(self) -> None:
+        """Commencer à écouter."""
         self._should_stop = False
 
         listen_thread = threading.Thread(target=self._listen)
@@ -19,6 +25,7 @@ class ScanListener:
         self._threads.add(listen_thread)
 
     def stop(self) -> None:
+        """Arrêter d'écouter."""
         self._should_stop = True
 
         for thread in self._threads:
@@ -26,6 +33,7 @@ class ScanListener:
         self._threads.clear()
 
     def _listen(self) -> None:
+        """écouter et attendre qu'un client se connecte."""
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(TIMEOUT)
         
@@ -36,6 +44,7 @@ class ScanListener:
         
         while not self._should_stop:
             try:
+                # Accepter un client
                 client_sock, client_addr = sock.accept()
 
                 handle_thread = threading.Thread(
@@ -49,6 +58,11 @@ class ScanListener:
                 pass
         
     def _handle_client(self, client: socket.socket, client_ip: str) -> None:
+        """
+        Prendre en charge un client.
+        :param client: Le socket du client.
+        :param client_ip: L'adresse ip du client.
+        """
         print(f"[SCAN LISTENER] {client_ip} connected")
 
         client.settimeout(TIMEOUT)
@@ -57,7 +71,7 @@ class ScanListener:
         while client_connected and not self._should_stop:
             try:
                 msg = client.recv(PACKET_SIZE).decode()
-                print(f"[SCAN LISTENER] {client_ip} sent {msg}")
+                print(f"[SCAN LISTENER] {client_ip} sent \"{msg}\"")
 
                 if msg == "":
                     print(f"[SCAN LISTENER] {client_ip} disconnected")
