6e6042949184022d53bc3308276ff50c41caa5b2
Yuesubi <mr.lukse@gmail.com>
2023-05-09 11:32:38 +0200

    Ajout de commentaires pour le scanner

--- src/game/networking/scanner.py
+++ src/game/networking/scanner.py
@@ -7,12 +7,15 @@ from .network import SELF_IP, NETMASK
 
 
 class Scanner:
+    """Classe qui scanne le réseau pour trouver un serveur ouvert."""
 
     def __init__(self) -> None:
+        """Constructeur."""
         self._threads: set[threading.Thread] = set()
         self._should_stop: bool = False
 
     def start(self) -> None:
+        """Commencer à scanner le réseau."""
         self._should_stop: bool = False
 
         scan_thread = threading.Thread(target=self._scan_all)
@@ -20,6 +23,7 @@ class Scanner:
         self._threads.add(scan_thread)
 
     def stop(self) -> None:
+        """Arrêter de force le scan."""
         self._should_stop: bool = True
 
         for thread in self._threads:
@@ -27,9 +31,13 @@ class Scanner:
         self._threads.clear()
     
     def _scan_all(self) -> None:
+        """Scanner tout le réseau."""
+
+        # Récupérer l'ip et le masque de sous réseau
         net_ip = Ip.from_dec_str(SELF_IP)
         netmask = Ip.from_dec_str(NETMASK)
 
+        # L'ip maximale et minimal
         ip = Ip.min_of_net(net_ip, netmask)
         last_ip = Ip.max_of_net(net_ip, netmask)
 
@@ -44,6 +52,10 @@ class Scanner:
             ip.id += 1
 
     def _try_connect(self, ip: str) -> None:
+        """
+        Tenter d'établir une connection avec une ip.
+        :param ip: L'ip à tester.
+        """
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(TIMEOUT)
 
